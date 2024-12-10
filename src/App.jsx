74451b41e06ef56030b97986472eeda9546e76f8
Rishabh Gurbani <rishabh.gurbani23@gmail.com>
2024-12-10 09:53:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
     <>
       <h1>Vite + React</h1>
       <h4>Hey my name is Rishabh</h4>
+      commit test
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is => {count}
