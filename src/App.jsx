0662c843c67ea58a0678e3ae8b40d8382b034fcc
Rishabh Gurbani <rishabh.gurbani23@gmail.com>
2024-12-11 17:59:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
       <h1>Vite + React</h1>
       <h4>Hey my name is Rishabh</h4>
       update
+      changing on prod
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is => {count}
